22f741967138f1b2d5efc9c8b5d5ef85c02ed634
Benjamin Heimann <benjamin.heimann@senacor.com>
2025-09-25 20:10:56 +0200

    Add Spring Boot skeleton

---
+++ src/main/java/de/insurance/vehiclepricing/VehiclepricingApplication.java
@@ -0,0 +1,13 @@
+package de.insurance.vehiclepricing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclepricingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiclepricingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/insurance/vehiclepricing/VehiclepricingApplicationTests.java
@@ -0,0 +1,13 @@
+package de.insurance.vehiclepricing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiclepricingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
