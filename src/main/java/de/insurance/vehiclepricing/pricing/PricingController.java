0277f1283d01319f0ea52d4d08232886f46c640b
Benjamin Heimann <benjamin.heimann@outlook.de>
2025-09-28 16:07:23 +0200

    Allow CORS

--- src/main/java/de/insurance/vehiclepricing/pricing/PricingController.java
+++ src/main/java/de/insurance/vehiclepricing/pricing/PricingController.java
@@ -3,12 +3,14 @@ package de.insurance.vehiclepricing.pricing;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/pricing")
 @RequiredArgsConstructor
 class PricingController {
